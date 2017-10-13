bfa5c979705e6f79d061d95bbe0a203283e84d4e
Nate Klein <32604753+nateklein@users.noreply.github.com>
2017-10-12 22:12:24 -0400

    Added data saving through Database for prematch values

--- app/src/main/java/robo/scoutingappmockup/goToPrematch.java
+++ app/src/main/java/robo/scoutingappmockup/goToPrematch.java
@@ -2,22 +2,36 @@ package robo.scoutingappmockup;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class goToPrematch extends Activity {
+
+    // Input fields
+    private EditText scoutNameText;
+    private CheckBox noShowBox;
 
     // Displays prematch page on activity call
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.prematch_page);
+        // Creates and sets data trackers to Database values
+        scoutNameText = (EditText) findViewById(R.id.scoutName);
+        scoutNameText.setText(MainActivity.db.scoutName);
+        noShowBox = (CheckBox) findViewById(R.id.noShow);
+        noShowBox.setChecked(MainActivity.db.noShow);
+
     }
 
-    // Calls activity to to auto page
+    // Calls activity to go to auto page
     public void toAuto(View v) {
+        // Saves values to Database
+        MainActivity.db.scoutName = scoutNameText.getText().toString();
+        MainActivity.db.noShow = noShowBox.isChecked();
+        // Switches pages
         Intent toAuto = new Intent(this, goToAuto.class);
         startActivity(toAuto);
     }

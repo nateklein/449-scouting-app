c9ffac6dcff3c054a4c0e154acada68583ec090d
Nate Klein <32604753+nateklein@users.noreply.github.com>
2017-10-15 23:00:43 -0400

    Added dropdown menu functionality and data saving

--- app/src/main/java/robo/scoutingappmockup/goToPrematch.java
+++ app/src/main/java/robo/scoutingappmockup/goToPrematch.java
@@ -4,14 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 
-public class goToPrematch extends Activity {
+public class goToPrematch extends Activity implements AdapterView.OnItemSelectedListener{
 
     // Input fields
     private EditText scoutNameText;
     private CheckBox noShowBox;
+    private Spinner teamNumber;
+    private ArrayAdapter<CharSequence> teamAdapter;
+    private Spinner matchNumber;
+    private ArrayAdapter<CharSequence> matchAdapter;
 
     // Displays prematch page on activity call
     @Override
@@ -23,7 +30,40 @@ public class goToPrematch extends Activity {
         scoutNameText.setText(MainActivity.db.scoutName);
         noShowBox = (CheckBox) findViewById(R.id.noShow);
         noShowBox.setChecked(MainActivity.db.noShow);
+        teamNumber = (Spinner) findViewById(R.id.teamNumber);
+        teamAdapter = ArrayAdapter.createFromResource(this, R.array.teams, android.R.layout.simple_spinner_item);
+        teamAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        teamNumber.setAdapter(teamAdapter);
+        teamNumber.setOnItemSelectedListener(this);
+        teamNumber.setSelection(MainActivity.db.teamNumber);
+        matchNumber = (Spinner) findViewById(R.id.matchNumber);
+        matchAdapter = ArrayAdapter.createFromResource(this, R.array.matches, android.R.layout.simple_spinner_item);
+        matchAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        matchNumber.setAdapter(matchAdapter);
+        matchNumber.setOnItemSelectedListener(this);
+        matchNumber.setSelection(MainActivity.db.matchNumber);
+    }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        // Checks which dropdown was selected and changes the Database value accordingly
+        if (parent.getId() == R.id.matchNumber) {
+            MainActivity.db.matchNumber = pos;
+        }
+        else if (parent.getId() == R.id.teamNumber) {
+            MainActivity.db.teamNumber = pos;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Checks which dropdown was selected and changes the Database value accordingly
+        if (parent.getId() == R.id.matchNumber) {
+            MainActivity.db.matchNumber = 0;
+        }
+        else if (parent.getId() == R.id.teamNumber) {
+            MainActivity.db.teamNumber = 0;
+        }
     }
 
     // Calls activity to go to auto page

f6241ff1a9e73db11eaf6ee21cf93e4cf4c0bafb
Nate Klein <32604753+nateklein@users.noreply.github.com>
2018-01-23 22:07:47 -0500

    Removed game specific fields

--- app/src/main/java/robo/scoutingappmockup/goToAuto.java
+++ app/src/main/java/robo/scoutingappmockup/goToAuto.java
@@ -1,27 +1,15 @@
-package robo.scoutingappmockup;
+package robo.scoutingappframe;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 
 public class goToAuto extends Activity {
 
     // Data trackers
-    private int autoHighFuel;
-    private int autoLowFuel;
-
     // Text fields that display current value
-    private TextView autoHighFuelText;
-    private TextView autoLowFuelText;
-
     // Input fields
-    private CheckBox noAutoBox;
-    private CheckBox movedForwardBox;
-    private RadioGroup autoGear;
 
     // Displays auto page on activity call
     @Override
@@ -29,115 +17,11 @@ public class goToAuto extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.autonomous_page);
         // Create and set data trackers to values from Database
-        autoHighFuel = MainActivity.db.autoHighFuel;
-        autoLowFuel = MainActivity.db.autoLowFuel;
-        autoHighFuelText = (TextView) findViewById(R.id.autoHighFuel);
-        autoHighFuelText.setText(Integer.toString(autoHighFuel));
-        autoLowFuelText = (TextView) findViewById(R.id.autoLowFuel);
-        autoLowFuelText.setText(Integer.toString(autoLowFuel));
-        noAutoBox = (CheckBox) findViewById(R.id.noAuto);
-        noAutoBox.setChecked(MainActivity.db.noAuto);
-        movedForwardBox = (CheckBox) findViewById(R.id.movedForward);
-        movedForwardBox.setChecked(MainActivity.db.movedForward);
-        autoGear = (RadioGroup) findViewById(R.id.autoGear);
-        switch (MainActivity.db.autoGear) {
-            case 0:
-                autoGear.clearCheck();
-                break;
-            case 1:
-                autoGear.check(R.id.autoGearNone);
-                break;
-            case 2:
-                autoGear.check(R.id.autoGearMissed);
-                break;
-            case 3:
-                autoGear.check(R.id.autoGearCenter);
-                break;
-            case 4:
-                autoGear.check(R.id.autoGearBoiler);
-                break;
-            case 5:
-                autoGear.check(R.id.autoGearLoading);
-                break;
-        }
-    }
-
-    // Fuel adding and subtracting
-    public void autoHighFuelPlus1(View v) {
-        autoHighFuel++;
-        autoHighFuelText.setText(Integer.toString(autoHighFuel));
-    }
-
-    public void autoHighFuelPlus5(View v) {
-        autoHighFuel+=5;
-        autoHighFuelText.setText(Integer.toString(autoHighFuel));
-    }
-
-    public void autoHighFuelMinus1(View v) {
-        if (autoHighFuel > 0) {
-            autoHighFuel--;
-            autoHighFuelText.setText(Integer.toString(autoHighFuel));
-        }
-    }
-
-    public void autoHighFuelMinus5(View v) {
-        if (autoHighFuel > 4) {
-            autoHighFuel-=5;
-            autoHighFuelText.setText(Integer.toString(autoHighFuel));
-        }
-    }
-
-    public void autoLowFuelPlus1(View v) {
-        autoLowFuel++;
-        autoLowFuelText.setText(Integer.toString(autoLowFuel));
-    }
-
-    public void autoLowFuelPlus5(View v) {
-        autoLowFuel+=5;
-        autoLowFuelText.setText(Integer.toString(autoLowFuel));
-    }
-
-    public void autoLowFuelMinus1(View v) {
-        if (autoLowFuel > 0) {
-            autoLowFuel--;
-            autoLowFuelText.setText(Integer.toString(autoLowFuel));
-        }
-    }
-
-    public void autoLowFuelMinus5(View v) {
-        if (autoLowFuel > 4) {
-            autoLowFuel-=5;
-            autoLowFuelText.setText(Integer.toString(autoLowFuel));
-        }
     }
 
     // Calls activity to go to teleop page
     public void toTeleop(View v) {
         // Save values into Database
-        MainActivity.db.autoLowFuel = autoLowFuel;
-        MainActivity.db.autoHighFuel = autoHighFuel;
-        MainActivity.db.noAuto = noAutoBox.isChecked();
-        MainActivity.db.movedForward = movedForwardBox.isChecked();
-        switch (autoGear.getCheckedRadioButtonId()) {
-            case R.id.autoGearNone:
-                MainActivity.db.autoGear = 1;
-                break;
-            case R.id.autoGearMissed:
-                MainActivity.db.autoGear = 2;
-                break;
-            case R.id.autoGearCenter:
-                MainActivity.db.autoGear = 3;
-                break;
-            case R.id.autoGearBoiler:
-                MainActivity.db.autoGear = 4;
-                break;
-            case R.id.autoGearLoading:
-                MainActivity.db.autoGear = 5;
-                break;
-            default:
-                MainActivity.db.autoGear = 0;
-                break;
-        }
         // Switch pages
         Intent toTeleop = new Intent(this, goToTeleop.class);
         startActivity(toTeleop);
@@ -145,31 +29,7 @@ public class goToAuto extends Activity {
 
     // Calls activity to go to prematch page
     public void toPrematch(View v) {
-        // Saves values to Database
-        MainActivity.db.autoLowFuel = autoLowFuel;
-        MainActivity.db.autoHighFuel = autoHighFuel;
-        MainActivity.db.noAuto = noAutoBox.isChecked();
-        MainActivity.db.movedForward = movedForwardBox.isChecked();
-        switch (autoGear.getCheckedRadioButtonId()) {
-            case R.id.autoGearNone:
-                MainActivity.db.autoGear = 1;
-                break;
-            case R.id.autoGearMissed:
-                MainActivity.db.autoGear = 2;
-                break;
-            case R.id.autoGearCenter:
-                MainActivity.db.autoGear = 3;
-                break;
-            case R.id.autoGearBoiler:
-                MainActivity.db.autoGear = 4;
-                break;
-            case R.id.autoGearLoading:
-                MainActivity.db.autoGear = 5;
-                break;
-            default:
-                MainActivity.db.autoGear = 0;
-                break;
-        }
+        // Save values to Database
         // Switches pages
         Intent toPrematch = new Intent(this, goToPrematch.class);
         startActivity(toPrematch);
